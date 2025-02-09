03e551ddcac5416fb6dd79dba3f4ee0a6c0a12f7
Nuraly <abdrashevnuraly72@gmail.com>
2025-02-09 13:36:41 +0500

    fund part 3

---
+++ lab 3/task1/JavaScript Fundamentals/14.js
@@ -0,0 +1,3 @@
+let a = 2, b = 1;
+let result = (a + b < 4) ? 'Below' : 'Over';
+console.log(result);
---
+++ lab 3/task1/JavaScript Fundamentals/15.js
@@ -0,0 +1,6 @@
+let login = 'Director';
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
+console.log(message);
